08b0ecc75e698c38c2a1a06ae25620b7c134dd8a
younglherme <guilhermeluizartes@hotmail.com>
2025-11-13 18:49:34 -0300

    subindo aula

---
+++ habilidadesEquipamentos/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export const App = () => {
+  return <View style={styles.container}>
+  </View>
+}
